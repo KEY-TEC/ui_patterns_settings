f17e781527a54d69c7c854444b0218461d6674d0
Christian Wiedemann <christian.wiedemann@key-tec.de>
2021-11-04 21:37:07 +0100

    Add support for image styles and responsive image styles

--- src/Plugin/UiPatterns/SettingType/MediaLibrarySettingType.php
+++ src/Plugin/UiPatterns/SettingType/MediaLibrarySettingType.php
@@ -27,28 +27,57 @@ class MediaLibrarySettingType extends PatternSettingTypeBase {
    * {@inheritdoc}
    */
   public function settingsPreprocess($value, array $context, PatternDefinitionSetting $def) {
+    if (intval($value)) {
+      $media_id = $value;
+      $media = Media::load($media_id);
+    }
+    else {
+      return ['#type' => 'markup', '#markup' => ''];
+    }
+
+    $image_style = $def->getValue('image_style');
+    if (empty($image_style) === FALSE) {
+      $image_uri = $media->image->entity !== NULL ? $media->image->uri : NULL;
+      if ($image_uri !== NULL) {
+        return [
+          '#theme' => 'image_style',
+          '#style_name' => $image_style,
+          '#uri' => $image_uri,
+        ];
+      }
+    }
+
+    $responsive_image_style = $def->getValue('responsive_image_style');
+    if (empty($responsive_image_style) === FALSE) {
+      $image_uri = $media->image->entity !== NULL ? $media->image->uri : NULL;
+      if ($image_uri !== NULL) {
+        return [
+          '#theme' => 'responsive_image_style',
+          '#responsive_image_style_id' => $image_style,
+          '#uri' => $image_uri,
+        ];
+      }
+    }
+
     $view_mode = $def->getValue('view_mode');
     if (empty($view_mode)) {
       return $value;
     }
-    elseif (intval($value)) {
-      $media_id = $value;
-      $media = Media::load($media_id);
-      if ($media !== NULL) {
-        $view_mode_builder = $this->entityTypeManager->getViewBuilder('media');
-        return $view_mode_builder->view($media, $view_mode);
-      }
-      else {
-        return '';
-      }
-    }
+    $view_mode_builder = $this->entityTypeManager->getViewBuilder('media');
+    return $view_mode_builder->view($media, $view_mode);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fieldStorageExposableTypes() {
+    return ['media'];
   }
 
   /**
    * {@inheritdoc}
    */
   public function settingsForm(array $form, $value, PatternDefinitionSetting $def, $form_type) {
-
     $form[$def->getName()] = [
       '#type' => 'media_library',
       '#title' => $def->getLabel(),
